2f78c8e7566e42cfde232cb63483796e095783d5
陈美安 <2966497308@qq.com>
2021-05-16 17:51:01 +0800

    更新代码

--- flutter_cityshop_store/lib/pages/mine_pages/mine_pages.dart
+++ flutter_cityshop_store/lib/pages/mine_pages/mine_pages.dart
@@ -28,7 +28,6 @@ class MyHomePage extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
@@ -50,12 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: ThemeColors.mainBgColor,
       appBar: AppBar(
@@ -64,14 +57,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      // body: BlocProvider(
-      //   create: (BuildContext context) => InfoBloc(),
-      //   child: Center(
-      //     // Center is a layout widget. It takes a single child and positions it
-      //     // in the middle of the parent.
-      //     child:InCounter() ,
-      //   ),
-      // ),
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
