58cd7b80ce8b932a0d14e9884a61094cfce2a9d8
chenmeian <2966497308@qq.com>
2021-11-26 12:37:40 +0800

    更新文件代码

--- flutter_cityshop_store/lib/pages/login/login_page.dart
+++ flutter_cityshop_store/lib/pages/login/login_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+
 class LoginPage extends StatelessWidget {
+  LoginPage() {
+    print("LoginPage-构造函数被调用");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,4 +23,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
