893824279ca36a33214d28794a0c5b4454ffaf14
chenmeian <2966497308@qq.com>
2021-11-26 16:43:25 +0800

    Android集成um统计

--- flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
+++ flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
@@ -3,15 +3,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import io.flutter.app.FlutterApplication;
-
+import com.umeng.commonsdk.UMConfigure;
 
 
 public class MainApplication extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        // UMConfigure.init(this, "61a07a2fe014255fcb8d33bc", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
-    	// UMConfigure.setLogEnabled(true);
+        UMConfigure.init(this, "61a07a2fe014255fcb8d33bc", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+    	UMConfigure.setLogEnabled(true);
     	android.util.Log.i("UMLog", "UMConfigure.init@MainApplication");
     }
 }
