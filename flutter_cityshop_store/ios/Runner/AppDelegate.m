b7756f9e2f4fb4a507f51fbdda306d6144558783
chenmeian <2966497308@qq.com>
2021-11-26 17:12:11 +0800

    更新iOS文件友盟

--- flutter_cityshop_store/ios/Runner/AppDelegate.m
+++ flutter_cityshop_store/ios/Runner/AppDelegate.m
@@ -1,5 +1,9 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+// 友盟统计sdk
+#import <UMCommon/UMConfigure.h>
+#import <UMCommon/MobClick.h>
+
 
 @implementation AppDelegate
 
@@ -7,7 +11,14 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [self initAnalysisUM];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+
+-(void)initAnalysisUM{
+    [UMConfigure setLogEnabled:YES];
+    [UMConfigure initWithAppkey:@"61a07941e014255fcb8d32ac" channel:@"App Store"]; // 增加这行
 }
 
 @end
