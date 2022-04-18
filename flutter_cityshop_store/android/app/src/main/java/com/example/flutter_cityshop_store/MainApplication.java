ca2f24c557dc65083b78caca3d6ce269f00e94c2
chenmeian <2966497308@qq.com>
2022-04-18 16:12:16 +0800

    校验是否第一次安装

--- flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
+++ flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
@@ -4,14 +4,20 @@ import java.util.List;
 import java.util.Map;
 import io.flutter.app.FlutterApplication;
 import com.umeng.commonsdk.UMConfigure;
-
+import android.content.SharedPreferences;
 
 public class MainApplication extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        UMConfigure.init(this, "61a07a2fe014255fcb8d33bc", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
-    	UMConfigure.setLogEnabled(true);
-    	android.util.Log.i("UMLog", "UMConfigure.init@MainApplication");
+
+        SharedPreferences sp = getSharedPreferences("FlutterSharedPreferences", this.MODE_PRIVATE);
+        String policy = sp.getString("policy","");
+        if (policy.equals("1")){
+            UMConfigure.init(this, "61a07a2fe014255fcb8d33bc", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
+            UMConfigure.setLogEnabled(true);
+            android.util.Log.i("UMLog", "UMConfigure.init@MainApplication");
+        }
+
     }
 }
