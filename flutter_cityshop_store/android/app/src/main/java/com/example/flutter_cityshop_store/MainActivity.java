7ec6d71aa12c2a72db6c9322527768387e1fc381
chenmeian <2966497308@qq.com>
2022-04-19 15:26:23 +0800

    删除UM统计

--- flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainActivity.java
+++ flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import com.umeng.analytics.MobclickAgent;
+
 
 public class MainActivity extends FlutterActivity {
 
@@ -17,22 +17,19 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MobclickAgent.setSessionContinueMillis(1000*40);
-        android.util.Log.i("UMLog", "onCreate@MainActivity");
+   
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        MobclickAgent.onPause(this);
-        android.util.Log.i("UMLog", "onPause@MainActivity");
+        
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MobclickAgent.onResume(this);
-        android.util.Log.i("UMLog", "onResume@MainActivity");
+       
     }
 
 }
--- flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
+++ flutter_cityshop_store/android/app/src/main/java/com/example/flutter_cityshop_store/MainApplication.java
@@ -3,21 +3,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import io.flutter.app.FlutterApplication;
-import com.umeng.commonsdk.UMConfigure;
-import android.content.SharedPreferences;
+
 
 public class MainApplication extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        SharedPreferences sp = getSharedPreferences("FlutterSharedPreferences", this.MODE_PRIVATE);
-        String policy = sp.getString("policy","");
-        if (policy.equals("1")){
-            UMConfigure.init(this, "61a07a2fe014255fcb8d33bc", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, null);
-            UMConfigure.setLogEnabled(true);
-            android.util.Log.i("UMLog", "UMConfigure.init@MainApplication");
-        }
 
     }
 }
