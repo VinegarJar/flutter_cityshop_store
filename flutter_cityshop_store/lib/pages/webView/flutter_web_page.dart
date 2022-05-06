391aeae9d23d5941a350fecdd8c4ed8c5dd2fff2
chenmeian <2966497308@qq.com>
2022-05-06 14:38:26 +0800

    更新app跳转下载

--- flutter_cityshop_store/lib/pages/webView/flutter_web_page.dart
+++ flutter_cityshop_store/lib/pages/webView/flutter_web_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cityshop_store/utils/themecolors.dart';
-import 'package:flutter_cityshop_store/widget/loding.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FlutterWebPage extends StatefulWidget {
   final String resultUrl;
@@ -13,31 +13,60 @@ class FlutterWebPage extends StatefulWidget {
   _FlutterWebPageState createState() => _FlutterWebPageState();
 }
 
+//https://cloud.tencent.com/developer/ask/sof/736876/answer/1078726 webveiwe下载apk参考地址
 class _FlutterWebPageState extends State<FlutterWebPage> {
+  InAppWebViewController webView;
+
   @override
   Widget build(BuildContext context) {
-    return (WebviewScaffold(
-        mediaPlaybackRequiresUserGesture: true,
-        url: widget.resultUrl,
-        appBar: AppBar(
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios, color: ThemeColors.titlesColor),
-              onPressed: () {
-                Navigator.pop(context);
-              }),
-          centerTitle: true,
-          backgroundColor: ThemeColors.homemainColor, //标题居中显示
-        ),
-        initialChild: Container(
-          color: Colors.white,
-          child: Center(child: LoadingWidget()),
-        )));
+    return (Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: ThemeColors.titlesColor),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        centerTitle: true,
+        backgroundColor: ThemeColors.homemainColor, //标题居中显示
+      ),
+      body: InAppWebView(
+          initialUrl: widget.resultUrl,
+          initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(
+                mediaPlaybackRequiresUserGesture: false,
+                debuggingEnabled: true,
+                useOnDownloadStart: true),
+          ),
+          onWebViewCreated: (InAppWebViewController controller) {
+            webView = controller;
+          },
+          onLoadStart: (InAppWebViewController controller, String url) {},
+          onLoadStop: (InAppWebViewController controller, String url) {},
+          onDownloadStart:
+              (InAppWebViewController controller, String url) async {
+            print("开始下载--------------->$url");
+            launch(url);
+          },
+          onReceivedServerTrustAuthRequest: (controller, challenge) async {
+            return ServerTrustAuthResponse(
+                action: ServerTrustAuthResponseAction.PROCEED);
+          },
+          androidOnPermissionRequest: (InAppWebViewController controller,
+              String origin, List<String> resources) async {
+            return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT);
+          }),
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   void dispose() {
-    // 回收相关资源
-    // Every listener should be canceled, the same should be done with this stream.
     super.dispose();
   }
 }
